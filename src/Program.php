9a9d521b59b8b8e23a8cfa13fb39b76a94df6a06
Jan Malisz <mistyfiky@gmail.com>
2023-11-16 20:13:05 +0100

    dodaj funkcję oznaczMiejscaBomb

--- src/Program.php
+++ src/Program.php
@@ -14,12 +14,40 @@ class Program
 
     public function programuj(): array
     {
+        $wyzerowanaTablica = $this->stwórzWyzerowanąTablicę();
+        $oznaczonaTablica = $this->oznaczMiejscaBomb($wyzerowanaTablica);
+
+
         return [
             ['*', '1', '0', '0'],
             ['2', '2', '1', '0'],
             ['1', '*', '1', '0'],
             ['1', '1', '1', '0'],
         ];
+    }
+
+    private function stwórzWyzerowanąTablicę(): array
+    {
+        $wyzerowanaTablica = [];
+        for ($wiersz = 0; $wiersz < $this->liczbaWierszy; $wiersz++) {
+            $wyzerowanaTablica[$wiersz] = array_fill(0, $this->liczbaKolumn, '0');
+        }
+
+        return $wyzerowanaTablica;
+    }
+
+    private function oznaczMiejscaBomb(array $wyzerowanaTablica): array
+    {
+        $oznaczonaTablica = $wyzerowanaTablica;
+        for ($wiersz = 0; $wiersz < $this->liczbaWierszy; $wiersz++) {
+            for ($kolumna = 0; $kolumna < $this->liczbaKolumn; $kolumna++) {
+                if ($wyzerowanaTablica[$wiersz][$kolumna] === '*') {
+                    $oznaczonaTablica[$wiersz][$kolumna] = '*';
+                }
+            }
+        }
+
+        return $oznaczonaTablica;
     }
 }
 
